65e93eb3ccbce2de14647cfbc2901c05bc335af0
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-29 20:36:18 -0300

    [two sum][python] add dictionary solution.

--- two_sum/sum_of_pair_equal_to_target/python/test_two_sum.py
+++ two_sum/sum_of_pair_equal_to_target/python/test_two_sum.py
@@ -1,22 +1,35 @@
 import unittest
 
-from two_pointers import two_sum
+from two_sum import two_sum_1, two_sum_2
 
 
 class TestTwoSum(unittest.TestCase):
 
-    def test_two_sum(self):
+    def test_two_sum_1(self):
         arr = [10, 20, 35, 50]
         target = 70
-        self.assertTrue(two_sum(arr, target))
+        self.assertTrue(two_sum_1(arr, target))
 
         arr = [10, 20, 30]
         target = 70
-        self.assertFalse(two_sum(arr, target))
+        self.assertFalse(two_sum_1(arr, target))
 
         arr = [-8, 1, 4, 6, 10, 45]
         target = 16
-        self.assertTrue(two_sum(arr, target))
+        self.assertTrue(two_sum_1(arr, target))
+
+    def test_two_sum_2(self):
+        arr = [10, 20, 35, 50]
+        target = 70
+        self.assertTrue(two_sum_2(arr, target))
+
+        arr = [10, 20, 30]
+        target = 70
+        self.assertFalse(two_sum_2(arr, target))
+
+        arr = [-8, 1, 4, 6, 10, 45]
+        target = 16
+        self.assertTrue(two_sum_2(arr, target))
 
 
 if __name__ == "__main__":
--- two_sum/sum_of_pair_equal_to_target/python/two_sum.py
+++ two_sum/sum_of_pair_equal_to_target/python/two_sum.py
@@ -10,7 +10,8 @@ Output: true ([20, 50] add up to 70)
 """
 
 
-def two_sum(arr: list[int], target: int) -> bool:
+# two_sum_1 uses the `two pointers` technique.
+def two_sum_1(arr: list[int], target: int) -> bool:
     left, right = 0, len(arr) - 1
 
     while left < right:
@@ -22,5 +23,19 @@ def two_sum(arr: list[int], target: int) -> bool:
             left += 1
         else:
             right -= 1
+
+    return False
+
+
+# two_sum_2 uses the dictionary ((hash) map) technique.
+def two_sum_2(arr: list[int], target: int) -> bool:
+    complements: dict[int, bool] = {}
+
+    for n in arr:
+        if n in complements.keys():
+            return True
+
+        complement = target - n
+        complements[complement] = True
 
     return False
