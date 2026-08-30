e9f80a82160dd1b2f8efd942a803ecbc20b3ff25
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-29 21:00:32 -0300

    [two sum][rust] add hash map solution.

--- two_sum/sum_of_pair_equal_to_target/rust/two-sum/src/main.rs
+++ two_sum/sum_of_pair_equal_to_target/rust/two-sum/src/main.rs
@@ -6,7 +6,13 @@
 // Input: arr = [10, 20, 35, 50]; target = 70
 // Output: true ([20, 50] add up to 70)
 
-fn two_sum(arr: Vec<i32>, target: i32) -> bool {
+use std::collections::HashMap;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn two_sum_1(arr: Vec<i32>, target: i32) -> bool {
     let mut left = 0;
     let mut right = arr.len() - 1;
 
@@ -27,29 +33,60 @@ fn two_sum(arr: Vec<i32>, target: i32) -> bool {
     return false;
 }
 
-fn main() {
-    println!("Hello, world!");
+fn two_sum_2(arr: Vec<i32>, target: i32) -> bool {
+    let mut complements: HashMap<i32, bool> = HashMap::new();
+
+    for n in arr.iter() {
+        let res = complements.get(&n);
+        match res {
+            Some(_) => return true,
+            None => {
+                let comp = target - n;
+                complements.insert(comp, true);
+            }
+        }
+    }
+
+    return false;
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::two_sum;
+    use crate::*;
 
     #[test]
-    fn test_two_sum() {
+    fn test_two_sum_1() {
         let arr = vec![10, 20, 35, 50];
         let target = 70;
 
-        assert!(two_sum(arr, target));
+        assert!(two_sum_1(arr, target));
 
         let arr = vec![10, 20, 30];
         let target = 70;
 
-        assert!(!two_sum(arr, target));
+        assert!(!two_sum_1(arr, target));
 
         let arr = vec![-8, 1, 4, 6, 10, 45];
         let target = 16;
 
-        assert!(two_sum(arr, target));
+        assert!(two_sum_1(arr, target));
+    }
+
+    #[test]
+    fn test_two_sum_2() {
+        let arr = vec![10, 20, 35, 50];
+        let target = 70;
+
+        assert!(two_sum_2(arr, target));
+
+        let arr = vec![10, 20, 30];
+        let target = 70;
+
+        assert!(!two_sum_2(arr, target));
+
+        let arr = vec![-8, 1, 4, 6, 10, 45];
+        let target = 16;
+
+        assert!(two_sum_2(arr, target));
     }
 }
