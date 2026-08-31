ce7b7dc04d6f31060f526540a3b43911ed48e10c
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-31 19:10:53 -0300

    [two sum][cpp] format file.

--- two_sum/sum_of_pair_equal_to_target/cpp/two_sum.cpp
+++ two_sum/sum_of_pair_equal_to_target/cpp/two_sum.cpp
@@ -5,60 +5,71 @@
 auto two_sum_1(std::vector<int>, int) -> bool;
 auto two_sum_2(std::vector<int>, int) -> bool;
 
+auto main() -> int
+{
+    auto arr = std::vector<int>{10, 20, 30, 50};
+    auto target = 80;
 
-auto main() -> int {
-  auto arr = std::vector<int>{10, 20, 30, 50};
-  auto target = 80;
+    auto result = two_sum_1(arr, target);
 
-  auto result = two_sum_1(arr, target);
-
-  if (result) {
-    std::println("Pair found");
-  } else {
-    std::println("Not a sausage.");
-  }
-
-  target = 50;
-
-  result = two_sum_2(arr, target);
-
-  if (result) {
-    std::println("Pair found");
-  } else {
-    std::println("Not a sausage.");
-  }
-
-  return 0;
-}
-
-
-auto two_sum_1(std::vector<int> arr, int target) -> bool {
-  int left = 0;
-  int right = arr.size() - 1;
-
-  while (left < right) {
-    // TODO: this can overflow.
-    int sum = arr.at(left) + arr.at(right);
-
-    if (sum == target) return true;
-
-    if (sum < target)
-      left++;
+    if (result)
+    {
+        std::println("Pair found");
+    }
     else
-      right--;
-  }
+    {
+        std::println("Not a sausage.");
+    }
 
-  return false;
+    target = 50;
+
+    result = two_sum_2(arr, target);
+
+    if (result)
+    {
+        std::println("Pair found");
+    }
+    else
+    {
+        std::println("Not a sausage.");
+    }
+
+    return 0;
 }
 
-auto two_sum_2(std::vector<int> arr, int target) -> bool {
-  auto complements_map = std::unordered_map<int, bool>();
+auto two_sum_1(std::vector<int> arr, int target) -> bool
+{
+    int left = 0;
+    int right = arr.size() - 1;
 
-  for (auto& n : arr) {
-    if (complements_map.count(n) >= 1) return true;
+    while (left < right)
+    {
+        // TODO: this can overflow.
+        int sum = arr.at(left) + arr.at(right);
 
-    complements_map[target - n] = true;
-  }
+        if (sum == target)
+            return true;
 
-  return false;
+        if (sum < target)
+            left++;
+        else
+            right--;
+    }
+
+    return false;
+}
+
+auto two_sum_2(std::vector<int> arr, int target) -> bool
+{
+    auto complements_map = std::unordered_map<int, bool>();
+
+    for (auto& n : arr)
+    {
+        if (complements_map.count(n) >= 1)
+            return true;
+
+        complements_map[target - n] = true;
+    }
+
+    return false;
 }
