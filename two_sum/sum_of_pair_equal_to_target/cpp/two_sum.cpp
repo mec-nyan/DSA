b202e690f4ca9a943b2eef3b09f82f8fd9250c6f
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-08-31 18:15:18 -0300

    [two sum] add cpp scaffolding.

--- two_sum/sum_of_pair_equal_to_target/cpp/two_sum.cpp
+++ two_sum/sum_of_pair_equal_to_target/cpp/two_sum.cpp
@@ -1,7 +1,21 @@
 #include <print>
+#include <vector>
+
+auto two_sum_1(std::vector<int>, int) -> bool;
+auto two_sum_2(std::vector<int>, int) -> bool;
+
 
 auto main() -> int {
-  std::println("Hello, world!");
+  auto arr = std::vector<int>{10, 20, 30, 50};
+  auto target = 80;
+
+  auto result = two_sum_1(arr, target);
+
+  if (result) {
+    std::println("Pair found");
+  } else {
+    std::println("Not a sausage.");
+  }
 
   return 0;
 }
