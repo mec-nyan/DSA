2f507c00a8acd431c6236f6c6b53a897be9a0677
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-09-01 20:21:50 -0300

    [binary search][go] tests and implementation.

--- searching/bin_search/go/main.go
+++ searching/bin_search/go/main.go
@@ -1,9 +1,32 @@
 // main.go
 package main
 
-import "fmt"
-
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Printf("I 💖 Go!\n")
+}
+
+func binSearch(arr []int, n int) (int, error) {
+	left := 0
+	right := len(arr) - 1
+
+	for left <= right {
+		middle := (right-left)/2 + left
+
+		if arr[middle] == n {
+			return middle, nil
+		}
+
+		if arr[middle] > n {
+			right = middle - 1
+		} else {
+			left = middle + 1
+		}
+	}
+
+	return 0, errors.New(fmt.Sprintf("%d is not there ...", n))
 }
--- searching/bin_search/go/main_test.go
+++ searching/bin_search/go/main_test.go
@@ -1,13 +1,54 @@
 // main_test.go
 package main
 
-import "testing"
+import (
+	"errors"
+	"log"
+	"testing"
+)
 
+func TestBinSearch(t *testing.T) {
+	type result struct {
+		idx int
+		err error
+	}
 
-func TestSomething(t *testing.T) {
-	// Given
+	testCases := []struct {
+		name string
+		arr  []int
+		n    int
+		want result
+	}{
+		{
+			name: "Should find 4 at pos 3.",
+			arr:  []int{1, 2, 3, 4, 5},
+			n:    4,
+			want: result{
+				idx: 3,
+				err: nil,
+			},
+		},
+		{
+			name: "Should return error.",
+			arr:  []int{1, 2, 3, 4, 5},
+			n:    7,
+			want: result{
+				idx: 0,
+				err: errors.New("7 is not there ..."),
+			},
+		},
+	}
 
-	// When
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := binSearch(tc.arr, tc.n)
 
-	// Then
+			if got != tc.want.idx || err != nil && err.Error() != tc.want.err.Error() {
+				log.Fatalf("Test failed: %s", err)
+			}
+
+			log.Println("Test passed.")
+
+		})
+	}
 }
