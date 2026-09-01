3739c7fed9421fdaa13b3ddc7fb56b48568b9642
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-09-01 19:36:26 -0300

    [binary search][cpp] add failing test.

--- searching/bin_search/bin_search.cpp
+++ searching/bin_search/bin_search.cpp
@@ -1,10 +1,29 @@
 // bin_search.cpp
+#include <cstdint>
+#include <optional>
 #include <print>
+#include <vector>
 
-auto main() -> int {
+// Search for `n` in a sorted list of numbers.  If present, return its offset.
+auto bin_search(std::vector<int32_t>, int32_t) -> std::optional<int32_t>;
 
-	std::println("I 💖 C++!");
+auto main() -> int
+{
 
-	return 0;
+    auto elems = std::vector<int32_t>{10, 20, 32, 44};
 
+    auto n = 32;
+
+    auto is_in = bin_search(elems, n);
+
+    if (is_in.has_value())
+    {
+        std::println("{} found at offset {}", n, is_in.value());
+    }
+    else
+    {
+        std::println("{} was not found ...", n);
+    }
+
+    return 0;
 }
