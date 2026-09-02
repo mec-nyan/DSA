4ddbd678d1327aef0c4a881101ad13c062d74e9b
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-09-02 19:44:38 -0300

    [binary search][python] add basic solution.

--- searching/bin_search/python/bin_search.py
+++ searching/bin_search/python/bin_search.py
@@ -5,4 +5,18 @@ Binary search (simple implementation).
 
 
 def binary_search(arr: list[int], n: int) -> int:
-    return -100
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        middle = (right - left) // 2 + left
+        current = arr[middle]
+
+        if current == n:
+            return middle
+
+        if current < n:
+            left = middle + 1
+        else:
+            right = middle - 1
+
+    return -1
