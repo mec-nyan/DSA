bbf8ba6b8647f64e56eec0cba819276cbdd589b7
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-09-02 19:30:51 -0300

    [binary search][lua] add basic solution.

--- searching/bin_search/lua/bin_search.lua
+++ searching/bin_search/lua/bin_search.lua
@@ -5,16 +5,35 @@
 --]]
 
 local function binary_search(arr, n)
-  return 0, nil
+  local left, right = 1, #arr
+
+  while left <= right do
+    local middle = math.floor((right - left) / 2) + left
+
+    local current = arr[middle]
+
+    if current == n then
+      return middle, nil
+    end
+
+    if current < n then
+      left = middle + 1
+    else
+      right = middle - 1
+    end
+  end
+
+  return 0, 'Not found.'
+
 end
 
 local function test(name, fun)
 	local ok, err = pcall(fun)
 
 	if ok then
-		print('PASS: ' .. name)
+		print('PASS: ' .. name .. '\t\x1b[32mOk ✔\x1b[0m')
 	else
-		print('FAIL: ' .. name)
+		print('FAIL: ' .. name .. '\t\x1b[31m:( ✖\x1b[0m')
 		print('....  ' .. err)
 		os.exit(1)
 	end
@@ -23,7 +42,7 @@ end
 test('Test binary search', function()
   local test_cases = {
     {
-      name = 'Element is present.',
+      name = 'Should be found.',
       arr = {1, 2, 3, 4, 5},
       n = 4,
       want = {
@@ -32,7 +51,7 @@ test('Test binary search', function()
       },
     },
     {
-      name = 'Element isn\'t present.',
+      name = "There's nothing here ...",
       arr = {1, 2, 3, 4, 5},
       n = 7,
       want = {
@@ -44,8 +63,8 @@ test('Test binary search', function()
 
   for _, tc in pairs(test_cases) do
     print('==', tc.name)
-    index, error = binary_search(tc.arr, n)
-    assert(error == tc.want.error)
-    assert(index == tc.want.index)
+    index, error = binary_search(tc.arr, tc.n)
+    assert(error == tc.want.error, "Errors don't match!")
+    assert(index == tc.want.index, "Indices don't match!")
   end
 end)
